6e67954c6c1ee741262d61ab2d3c4546b6bc8aaf
Lucas Jesus <lucas.lsj1711@gmail.com>
2021-12-21 11:21:34 -0300

    retirando props

--- germinare-mui-components/src/components/UploadImage/UploadImage.tsx
+++ germinare-mui-components/src/components/UploadImage/UploadImage.tsx
@@ -4,16 +4,12 @@ import { Box } from '@mui/material'
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import { useState } from "react";
 import EditIcon from '@mui/icons-material/Edit';
-interface IUploadImageProp {
-    url?: string
-}
-export default function UploadImage(props: IUploadImageProp){
+export default function UploadImage(){
     const [Url, setUrl] = useState('')
     return(
-        <UploadImageStyle {...props}>
+        <UploadImageStyle>
             <input type="file" accept="image/*" onChange={(event:any)=>{
                     setUrl(URL.createObjectURL(event.target.files[0]))
-                    
                 }} />
             {Url.length > 0 ? <Image>
                 <Box>
