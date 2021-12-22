cb6bad73696787d5e295feb730fabeff1081ee46
Junior743 <carloshenriquesousajunior@gmail.com>
2021-12-22 10:17:30 -0300

    improving TextField

--- germinare-mui-components/src/components/Form/TextField/textField.tsx
+++ germinare-mui-components/src/components/Form/TextField/textField.tsx
@@ -2,17 +2,44 @@ import React from 'react'
 
 import TextField from '@mui/material/TextField'
 import { useController, UseControllerProps } from 'react-hook-form'
+import InputMask from 'react-input-mask'
 
 export interface ITextFieldComponentProp extends UseControllerProps<any> {
   label: string
-  width?: number | string
   fullWidth?: boolean
+  mask?: any
+  onChange?: (value: any) => void
+  onBlur?: (value: any) => void
 }
 
 export default function TextFieldComponent(props: ITextFieldComponentProp) {
+  /* 
+    Warning: A component is changing an uncontrolled input to be controlled. 
+    This is likely caused by the value changing from undefined to a defined value, 
+    which should not happen. Decide between using a controlled or uncontrolled input element for the lifetime of the component. 
+    More info: https://reactjs.org/link/controlled-components
+  */
   const { field } = useController(props)
 
-  return (
-    <TextField {...props} {...field} />
+  function onChange(value: any) {
+    if (props?.onChange) props.onChange(value)
+  }
+
+  function onBlur(value: any) {
+    if (props?.onBlur) props.onBlur(value)
+  }
+
+  const TextFieldMask = () => (
+    <InputMask mask={props.mask} onBlur={onBlur} onChange={onChange}>
+      <TextField
+        name={field.name}
+        ref={field.ref}
+        value={field.value}
+        label={props.label}
+        fullWidth={props.fullWidth}
+      />
+    </InputMask>
   )
+
+  return props?.mask ? <TextFieldMask /> : <TextField {...props} {...field} />
 }
